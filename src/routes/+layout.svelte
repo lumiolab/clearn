555a4a138b41a1b79b2337caaa73a33b04f7e065
Lunx-MayoProductions <xen24@outlook.de>
2025-11-09 14:03:21 +0100

    <.< made the seo a bit better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,16 +16,16 @@
 	<meta property="og:url" content="https://mariann-bounden-uneclectically.ngrok-free.dev/">
 	<meta property="og:type" content="website">
 	<meta property="og:title" content="FCG - CLearn">
-	<meta property="og:description" content="FCG Site">
-	<meta property="og:image" content="">
+	<meta property="og:description" content="CLearn ist eine LernPlattform von FCG. Wir bieten viele Arten zu lernen unteranderem auch kleinere Spiele.">
+	<meta property="og:image" content="https://clearn-iota.vercel.app/_app/immutable/assets/fcg.CUGjIfqq.png">
 
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image">
 	<meta property="twitter:domain" content="mariann-bounden-uneclectically.ngrok-free.dev">
 	<meta property="twitter:url" content="https://mariann-bounden-uneclectically.ngrok-free.dev/">
 	<meta name="twitter:title" content="FCG - CLearn">
-	<meta name="twitter:description" content="FCG Site">
-	<meta name="twitter:image" content="">
+	<meta name="twitter:description" content="CLearn ist eine LernPlattform von FCG. Wir bieten viele Arten zu lernen unteranderem auch kleinere Spiele.">
+	<meta name="twitter:image" content="https://clearn-iota.vercel.app/_app/immutable/assets/fcg.CUGjIfqq.png">
 </svelte:head>
 
 {@render children()}
